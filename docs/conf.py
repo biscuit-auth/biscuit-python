cd85b30fa48487fa542e1d3fe62fbcd8e2619ce6
Clement Delafargue <clement.delafargue.ext@outscale.com>
2023-06-05 15:22:20 +0200

    doc: initial sphinx setup

---
+++ docs/conf.py
@@ -0,0 +1,33 @@
+import pathlib
+import sys
+sys.path.insert(0, pathlib.Path(__file__).parents[2].resolve().as_posix())
+
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'biscuit-python'
+copyright = '2023, Clément Delafargue'
+author = 'Clément Delafargue'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'sphinx.ext.doctest',
+    'sphinx.ext.autodoc',
+    'sphinx.ext.autosummary'
+]
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
---
+++ docs/index.rst
@@ -0,0 +1,16 @@
+biscuit-python
+==============
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+   generated/biscuit_auth
+
+This library provides support for the biscuit auth platform in python.
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`search`
